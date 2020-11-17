527ad2edc2f7194107add7cf6fa013a171ce84cd
Simon Elicaste <simonelicaste@gmail.com>
2020-11-17 15:07:43 +0100

    routes resources added and show and edit html

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,19 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes do
+    resources :doses,
+    resources :recipe_categories
+  end
+
+  resources :ingredients
+
+  resources :categories
+
+  resources :reviews
+
+
+  # resources :meal_planners do
+  #   resources :meal_planners_recipes
+# end
 end
