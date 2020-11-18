e8e31e7a463d768dee15af1996e1b18808ee5720
Simon Elicaste <simonelicaste@gmail.com>
2020-11-18 16:42:19 +0100

    seed fixed and some links

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+
+  def index
+    @recipes = Recipe.all
+  end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
 
   resources :user_meal_planners
 
-  # resources :meal_planners do
+  resources :meal_planners
+  
   #   resources :meal_planners_recipes
   # end
 
