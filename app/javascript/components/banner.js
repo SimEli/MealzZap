2be40154b550989fa37128560ad2bbbbe7faeafd
Nina Tomic <ninatomic1703@gmail.com>
2020-11-26 16:44:14 +0000

    changes

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,11 +2,12 @@ import Typed from 'typed.js';
 
 
 const loadDynamicBannerText = () => {
+  if (document.querySelector('#banner-typed-text')){
   new Typed('#banner-typed-text', {
     strings: ["Welcome to MealzZap !"],
     typeSpeed: 50,
     loop: true
   });
 }
-
+}
 export { loadDynamicBannerText };
